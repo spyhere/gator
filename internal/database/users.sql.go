dd5067ccddc0f065b3869dbd1674daa257b13c8b
spyhere <gekav3@mail.ru>
2025-12-11 19:47:03 +0200

    Fix "001_users" migration to have name as not null

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -7,7 +7,6 @@ package database
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -28,7 +27,7 @@ type CreateUserParams struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Name      sql.NullString
+	Name      string
 }
 
 func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
