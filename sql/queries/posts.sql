43ba2e474abdb4de542dc386cf3c13da7598803b
spyhere <gekav3@mail.ru>
2025-12-13 16:56:21 +0200

    Create "CreatePost", "GetPostsForUser" queries

---
+++ sql/queries/posts.sql
@@ -0,0 +1,12 @@
+-- name: CreatePost :one
+INSERT INTO posts(id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES ($1, NOW(), NOW(), $2, $3, $4, NULL, $5)
+RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT * FROM posts
+WHERE feed_id IN (
+  SELECT feed_id FROM feed_follows
+  WHERE user_id = $1
+);
+
