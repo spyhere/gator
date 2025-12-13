76c9ff2660152f1faf6b033e5f2203c5de6a60d0
spyhere <gekav3@mail.ru>
2025-12-13 14:57:02 +0200

    Create "MarkFeedFetched" and "GetNextFeedToFetch" queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -18,3 +18,13 @@ ON users.id = feeds.user_id;
 -- name: GetFeedByUrl :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $2
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
